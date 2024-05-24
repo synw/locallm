bbd7a9207e69c0b09ca2a345aa8b6f69a213ae8d
synw <synwe@yahoo.com>
2024-05-24 13:38:48 +0200

    Minor fix in examples

--- packages/api/examples/ollama.js
+++ packages/api/examples/ollama.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { Lm } from "../dist/main.es.js";
 
-const model = "phi3";
+const model = "mistral:instruct";
 const template = "[INST] {prompt} [/INST]";
 
 async function main() {
