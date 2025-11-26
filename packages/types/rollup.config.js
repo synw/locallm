f495bbb9148f869e78c98e4f67ca6aa9bb261a79
synw <synwe@yahoo.com>
2025-11-26 14:41:06 +0100

    Update types package to version 0.6.2 with restructured type exports
    - Bump version from 0.6.1 to 0.6.2
    - Change main entry point from src/interfaces.ts to src/main.ts
    - Update types field in package.json from ./dist/interfaces.d.ts to ./dist/main.d.ts
    - Remove deprecated src/interfaces.ts file that was only exporting type aliases
    - Restructure type exports to use main.ts as the primary export file

--- packages/types/rollup.config.js
+++ packages/types/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/interfaces.ts',
+  input: 'src/main.ts',
   output: [
     {
       file: 'dist/main.js',
